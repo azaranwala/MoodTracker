82f8b420666c7ec9e6bb1776167da63fad4480de
zaranwala <mustufa.zaranwala@gmail.com>
2025-07-27 19:56:27 -0400

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Mustufa Zaranwala on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
